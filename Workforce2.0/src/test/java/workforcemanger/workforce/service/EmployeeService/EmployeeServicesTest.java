464f2b1e26e5813af90312b6b6aa7162f369d29c
mouadHILLALI <mauadtoto001@gmail.com>
2024-10-12 16:27:20 +0100

    W20-11 setting testing unit for employee services

--- Workforce2.0/src/test/java/workforcemanger/workforce/service/EmployeeService/EmployeeServicesTest.java
+++ Workforce2.0/src/test/java/workforcemanger/workforce/service/EmployeeService/EmployeeServicesTest.java
@@ -1,7 +1,29 @@
 package workforcemanger.workforce.service.EmployeeService;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import workforcemanger.workforce.dto.EmployeeDTO;
 
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
 class EmployeeServicesTest {
 
+    private EmployeeServices employeeServices;
+    @BeforeEach
+    void setUp() {
+        employeeServices = new EmployeeServices();
+    }
+    @Test
+    void updateEmployee() {
+        EmployeeDTO employee = new EmployeeDTO(1, "John Doe", "johndoe@example.com",
+                "1234 Elm Street", "Developer",
+                LocalDate.of(2020, 1, 1), 4000.0,
+                2, "123-45-6789", "1234567890",
+                LocalDate.of(1990, 1, 1));
+        EmployeeDTO updatedEmployee = employeeServices.update(employee);
+        assertNotNull(updatedEmployee);
+        assertEquals("John Doe" , updatedEmployee.getUserName());
+        assertEquals(1 , updatedEmployee.getId());
+    }
 }
